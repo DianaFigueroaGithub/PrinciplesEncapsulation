6d26c58b9448015e2a8433ca6765d353b1a80b7e
Diana F <Diafis02@gmail.com>
2023-03-04 10:19:18 -0500

    New notes addede

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -8,22 +8,15 @@ public class BankAccount {
         return ownerName;
     }
 
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
-    }
-
     public float getBalance() {
         return balance;
-    }
-
-    public void setBalance(float balance) {
-        this.balance = balance;
     }
 
     public BankAccount(String ownerName, float balance) {
         this.ownerName = ownerName;
         this.balance = balance;
     }
+
     //withdraw- to take money out of a bank account - withdrawal n.
     public boolean withdraw(float amountOut){
         boolean withdrawal = false;
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-
        BankAccount debitAccount = new BankAccount("Diana", 500) ;
        System.out.println(debitAccount.withdraw(500)?"withdrawal successful":"withdrawal failed, not enough money");
        System.out.println(debitAccount.payIn(1000)?"Balance updated":"Error, Please Contact the Admin");
        System.out.println(debitAccount.withdraw(2000)?"withdrawal successful":"withdrawal failed, not enough money");
 
+        System.out.println(" Bank Account Information ");
        System.out.println("Bank Account Owner: " + debitAccount.getOwnerName());
        System.out.println(("Current Balance: "+ debitAccount.getBalance()));
-
-
     }
-
-
-
 
 }
